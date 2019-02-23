59e21d71b185ff0172b8bed46624f34851cea6d9
Michael Chenevey <mchenevey7@gmail.com>
2019-02-23 00:13:35 -0500

    yuh

--- ardSerial.py
+++ ardSerial.py
@@ -43,6 +43,7 @@ def write_read(token, var=""):
     
 '''
 Joint references:
+  0: 
   1: head pitch
   2: 
 '''
@@ -55,7 +56,7 @@ if __name__ == '__main__':
     time.sleep(1)
     
     for i in range(10):
-        joint = 1
+        joint = 0
         for a in np.arange(0, 2 * math.pi, 0.2):
             serialWriteByte('l', [joint, math.sin(a) * 30])
             time.sleep(0.04)
