ec1bd1f9d6e151b55d36e3f41f4fdc35f95d4885
Michael Chenevey <mchenevey7@gmail.com>
2019-02-26 22:47:27 -0500

    trying l command

--- ardSerial.py
+++ ardSerial.py
@@ -8,37 +8,43 @@ import numpy as np
 
 
 ser = serial.Serial(
-    port='/dev/ttyS0',
-    baudrate=57600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
+  port='/dev/ttyS0',
+  baudrate=57600,
+  parity=serial.PARITY_NONE,
+  stopbits=serial.STOPBITS_ONE,
+  bytesize=serial.EIGHTBITS,
+  timeout=1
 )
 
 
 def serialWriteByte(token, var=""):
 
-    if token == 'c' or token == 't':  # data
-        instrStr = token + str(var[0]) + ',' + str(var[1]) + '\n'
-    elif token == 'l':  # use binary to reduce packet size
-        ser.write(('l' + str(len(var))).encode())
-        print(var)
-        instrStr = struct.pack('b' * len(var), *[int(x) for x in var])
-    elif token == 'w' or token == 'k' or token == 'm':
-        instrStr = (token + var + "\n").encode()
-    else:
-        instrStr = token
-    ser.write(instrStr)
+  if token == 'c' or token == 't':  # data
+    instrStr = token + str(var[0]) + ',' + str(var[1]) + '\n'
+  elif token == 'l':  # use binary to reduce packet size
+    ser.write(('l' + str(len(var))).encode())
+    print(var)
+    instrStr = struct.pack('b' * len(var), *[int(x) for x in var])
+  elif token == 'w' or token == 'k' or token == 'm':
+    instrStr = (token + var + "\n").encode()
+  else:
+    instrStr = token
+  ser.write(instrStr)
     
 def write_read(token, var=""):
-    serialWriteByte(token, var)
-    s = ""
-    while ser.in_waiting:
-        x = ser.readline()
-        if x != "":
-            s += "\n"
-    return s
+  serialWriteByte(token, var)
+  s = ""
+  while ser.in_waiting:
+    x = ser.readline()
+    if x != "":
+      s += "\n"
+  return s
+
+def tups_to_angles(angles):
+  lst = [0] * 16
+  for servo, angle in angles:
+    lst[servo] = angle
+  return lst
 
     
 '''
@@ -50,18 +56,19 @@ Joint references:
 '''
 if __name__ == '__main__':
 
-    print("Starting ardSerial...")
-    serialWriteByte('k',"sit")
-    time.sleep(1)
-    serialWriteByte('k', "zero")
-    time.sleep(1)
-    
-    for i in range(10):
-        joint = 3
-        for a in np.arange(0, 2 * math.pi, 0.2):
-            serialWriteByte('l', [joint, math.sin(a) * 30])
-            time.sleep(0.04)
-            
-    print("ardSerial finished")
+  print("Starting ardSerial...")
+  serialWriteByte('k',"sit")
+  time.sleep(1)
+  serialWriteByte('k', "zero")
+  time.sleep(1)
+  
+  for i in range(10):
+    joint = 3
+    for a in np.arange(0, 2 * math.pi, 0.2):
+      angles = [(0, math.sin(a) * 30)]
+      serialWriteByte('l', tup_to_angles(angles))
+      time.sleep(0.04)
+          
+  print("ardSerial finished")
             
             
